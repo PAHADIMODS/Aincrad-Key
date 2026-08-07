35cb65367f18f75cef1e7a8cecda6760fb55da30
PAHADIMODS <mahendraadhikari159@gmail.com>
2026-08-07 09:24:56 +0530

    Update Error.js

--- Error.js
+++ Error.js
@@ -1,5 +1,5 @@
 (function() {
     'use strict';
-    const _fb = 'https://PAHADIMODS.short.gy/yEqWCw';
+    const _fb = 'https://telegram.me/+xVEVeNBqwthiMjhl';
     window.location.replace(_fb);
 })();
